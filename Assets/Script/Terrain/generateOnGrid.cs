480ff53d97d96d18a49041b997c22108962b4426
liv <liu-1@gmx.de>
2025-08-31 18:20:43 +0200

    Generation using random or custom seed

--- Assets/Script/Terrain/generateOnGrid.cs
+++ Assets/Script/Terrain/generateOnGrid.cs
@@ -1,32 +1,42 @@
 using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
 
 public class generateOnGrid : MonoBehaviour
 {
     [Header("Grid Attributes")]
     [SerializeField] private int _gridSize;
     [SerializeField] private List<GameObject> _tiles = new List<GameObject>();
+    [Space]
+    [Tooltip("Seed used for level generation: Use 0 for random seed")]
     [SerializeField] private int _seed;
     private Grid _grid;
     private Vector3 _position = Vector3.zero;
-    private Quaternion _rotation = Quaternion.Euler(-90,0,0);
+    private Quaternion _rotation = Quaternion.Euler(-90, 0, 0);
     private double _crashSite;
     private double _targetPoint;
+    System.Random _seededRandom;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _grid = GetComponent<Grid>();
 
         if (_seed == 0)
         {
-            _seed = 1;
+            _seed = Random.Range(1, 100000000);
+        }
+        _seededRandom = new System.Random(_seed);
+
+
+        Debug.Log("Generated using seed: " + _seed);
+        for (int i = 1; i < 4; i++)
+        {
+            Debug.Log("Random Number " + i + ". = " + _seededRandom.Next(1, 100));
         }
 
-        _grid = GetComponent<Grid>();
+
+
 
         for (int posX = 0; posX < _gridSize; posX++)
         {
@@ -34,7 +44,7 @@ public class generateOnGrid : MonoBehaviour
             for (int posZ = 0; posZ < _gridSize; posZ++)
             {
                 _position.z = posZ * _grid.cellSize.z;
-                Instantiate(_tiles[0], _position, _rotation, transform);
+                Instantiate(_tiles[_seededRandom.Next(0, _tiles.Count)], _position, _rotation, transform);
             }
 
 
@@ -44,6 +54,6 @@ public class generateOnGrid : MonoBehaviour
 
     }
 
-    
+
 
 }
